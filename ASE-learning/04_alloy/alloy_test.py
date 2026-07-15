3baf29f10383f92000bb25540e840c2f594430c1
aaaaabg2025-art <aaaaabg2025@gmail.com>
2026-07-15 15:41:05 +0800

    重新上传readme

--- ASE-learning/04_alloy/alloy_test.py
+++ ASE-learning/04_alloy/alloy_test.py
@@ -4,7 +4,7 @@ cu = bulk("Cu", "fcc", a=3.6, cubic=True)
 
 for i, atom in enumerate(cu):#依次访问每个原子
     print(i, atom.symbol)
-    cu[0].symbol = "Ni"
+    cu[0].symbol = "Ni"#atoms[i].symbol = "新元素"
 
 
 write("CuNi.xyz", cu)
